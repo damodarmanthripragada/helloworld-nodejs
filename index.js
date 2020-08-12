baa22386cb4b460ae131914e32ed3c4cc3161640
damodarmanthripragada <67912021+damodarmanthripragada@users.noreply.github.com>
2020-08-12 12:57:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+  console.log('Hello world received a request, testing the req res.');
 
   const target = process.env.TARGET || 'World';
   res.send('Hello Damodar Manthripragada ${target}!');
