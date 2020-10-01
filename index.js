eda0927f162292edd66f3e18ef89ebc27afcb6b2
damodarmanthripragada <67912021+damodarmanthripragada@users.noreply.github.com>
2020-10-01 15:44:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request, testing the req res.');
 
   const target = process.env.TARGET || 'World';
-  res.send('Hello Damodar Manthripragada ${target}!');
+  res.send('Hello Damodar Manthripragada Welcome');
 });
 
 const port = process.env.PORT || 8080;
